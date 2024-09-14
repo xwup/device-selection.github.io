845df10c90f7e9cc69fe9745cd17f4f5f5bc67a2
周虹宇 <zhouhy1990@qq.com>
2024-09-14 09:22:39 +0800

    oldcommit

--- src/components/DeviceSelection.vue
+++ src/components/DeviceSelection.vue
@@ -37,22 +37,7 @@
             <v-text-field v-model="assetNumber" label="资产编号"></v-text-field>
           </v-col>
           <v-col style="padding: 0px; width: 20%; flex-basis:auto;">
-            <v-btn class="text-none mb-4" color="indigo-darken-3" @click="openCamara">拍条码
-              <v-overlay v-model="overlayUpDown" scroll-strategy="block" style="justify-content: center;">
-                <v-container width="100%" justify="center" style="margin-top: 30%;">
-                  <v-row align="center" justify="center">
-                    <v-col style="padding: 0px; text-align: center;height: 20%; width: 60%;">
-                      <video ref="videoRef" id="videoRef" autoplay width="100%"></video>
-                    </v-col>
-                  </v-row>
-                  <v-row align="center" justify="center">
-                    <v-col style="padding: 0px; text-align: center;">
-                      <v-btn class="text-none" color="indigo-darken-3" @click="catchThisPhoto">拍摄</v-btn>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-overlay>
-            </v-btn>
+            <v-btn class="text-none mb-4" color="indigo-darken-3" @click="parseBarcode">拍条码</v-btn>
           </v-col>
         </v-row>
       </v-container>
@@ -60,10 +45,10 @@
       <div class="d-flex justify-space-between mt-2">
         <v-btn @click="exportExcel" class="text-none mb-4" color="grey-lighten-3" size="x-large" variant="flat">导出</v-btn>
         <v-btn class="text-none mb-4" color="indigo-darken-3" size="x-large" variant="flat" type="submit">录入</v-btn>
-
       </div>
     </v-form>
   </v-sheet>
+  <scanVideo></scanVideo>
   <div id="error-message"></div>
 </template>
 
@@ -71,6 +56,7 @@
 import { ref, onMounted } from 'vue';
 import * as XLSX from 'xlsx';
 import { BrowserMultiFormatReader } from '@zxing/library';
+import scanVideo from './scanVideo.vue';
 
 export default {
   setup() {
@@ -82,8 +68,6 @@ export default {
     const messageBox = ref(false);
     const messageText = ref('');
     const snackColor = ref('');
-
-    const overlayUpDown = ref(false);
 
     // 存储录入的数据
     const recordedData = ref([]);
@@ -138,16 +122,6 @@ export default {
       },
     ];
 
-    // const assetNumberRules = [
-    //   value => {
-    //     if (value || value.match(/^[a-zA-Z0-9]+$/)) {
-    //       return true;
-    //     }
-
-    //     return '资产编号只能是字母和数字组合';
-    //   },
-    // ];
-
     const messageAlert = (text, type) => {
       messageBox.value = true;
       messageText.value = text;
@@ -196,93 +170,6 @@ export default {
       XLSX.writeFile(workbook, '机房设备信息.xlsx');
     };
 
-    // 打开摄像头
-    const openCamara = async () => {
-      overlayUpDown.value = true;
-
-      // 等待 videoRef 初始化完成
-      await waitForVideoReady();
-
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
-        const videoElement = document.getElementById('videoRef');
-        videoElement.srcObject = stream;
-        videoRef.value.srcObject = stream;
-        messageAlert('摄像头已打开……', tipAlert)
-      } catch (error) {
-        console.error("Failed to get camera access:", error);
-        messageAlert('摄像头打开失败！', errorAlert)
-        // 在页面上打印错误信息
-        document.getElementById('error-message').innerText = `摄像头打开失败！错误信息: ${error.message}`;
-
-      }
-    };
-
-    // 等待 videoRef 初始化完成
-    const waitForVideoReady = () => {
-      return new Promise((resolve) => {
-        const videoElement = document.getElementById('videoRef');
-        if (videoElement && videoElement.readyState >= videoElement.HAVE_CURRENT_DATA) {
-          resolve();
-        } else {
-          videoElement.addEventListener('loadedmetadata', resolve);
-        }
-      });
-    };
-
-    const catchThisPhoto = () => {
-      // 这里可以添加拍照逻辑
-      // 拍照逻辑
-      const videoElement = document.getElementById('videoRef');
-      const canvas = document.createElement('canvas');
-      canvas.width = videoElement.videoWidth;
-      canvas.height = videoElement.videoHeight;
-      canvas.getContext('2d').drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-
-      // 转换为Base64字符串
-      const photoData = canvas.toDataURL('image/jpeg');
-
-      // 关闭相机并隐藏遮罩层
-      stopCamera();
-
-      // 解析条形码
-      parseBarcode(photoData).then(barcodeValue => {
-        assetNumber.value = barcodeValue;
-      }).catch(error => {
-        console.error('Error parsing barcode:', error);
-      });
-    };
-
-    // 使用 @zxing/library 解析条形码
-    const parseBarcode = async (photoData) => {
-      const reader = new BrowserMultiFormatReader();
-      const result = await reader.decode(photoData);
-      return result.text;
-    };
-
-    const stopCamera = () => {
-      const videoElement = document.getElementById('videoRef');
-      if (videoElement && videoElement.srcObject) {
-        const tracks = videoElement.srcObject.getTracks();
-        tracks.forEach(track => track.stop());
-        videoElement.srcObject = null;
-      }
-      overlayUpDown.value = false;
-    };
-
-    // 在 DOM 渲染完成后确保 videoRef 已经初始化
-    onMounted(() => {
-      // 确保 DOM 已经渲染完毕
-      setTimeout(() => {
-        const videoElement = document.getElementById('videoRef');
-        if (videoElement) {
-          console.log('videoRef 已经初始化');
-        } else {
-          console.error('videoRef 未初始化');
-        }
-      }, 500);
-    });
-
     return {
       building,
       messageBox,
@@ -298,9 +185,6 @@ export default {
       deviceLayerRules,
       handleRecord,
       exportExcel,
-      openCamara,
-      catchThisPhoto,
-      overlayUpDown,
     };
   },
 };
